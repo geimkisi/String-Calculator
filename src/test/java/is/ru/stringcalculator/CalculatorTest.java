f5213545c78566de28205c611d6f3ded59d3c7ef
geimkisi <geimkisi@gmail.com>
2015-10-18 19:41:19 +0000

    Now we can handle newlines
    there are 2 test cases
    one newline and one comma
    two newlines and no comma

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -25,8 +25,17 @@ public class CalculatorTest {
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
-    }
+	public void testMultipleNumbers(){
+	 	assertEquals(6, Calculator.add("1,2,3"));
+	 }
 
+	@Test
+	public void testWithNewLines(){
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void testWithMultibleNewLines(){
+		assertEquals(6, Calculator.add("1\n2\n3"));
+	}
 }
